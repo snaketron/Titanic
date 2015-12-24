bbb4d94ed2d86c6ff55709f3ba51439d5cd44208
simo <simo.kitanovski@uni-due.de>
2015-12-24 21:25:01 +0100

    end

--- titanicSVM.R
+++ titanicSVM.R
@@ -4,17 +4,18 @@ test <- read.csv(file = "test.csv")
 
 require("e1071")
 require("randomForest")
-require("snow")
-require("doParallel")
-require("foreach")
-require("doMC")
+require("plyr")
 
 train$Survived <- as.factor(train$Survived)
 test$Survived <- NA
 test$Survived <- as.factor(test$Survived)
 
-pass.id <- test$PassengerId
 
+p <- read.csv(file = "test.csv")
+p$Survived <- NA
+p$Survived <- as.factor(p$Survived)
+pass.id <- p$PassengerId
+rm(p)
 
 
 
@@ -26,6 +27,8 @@ for(d in decks) {
   train$Cabin[which(grepl(pattern = paste(d, "+", sep = ''), x = as.character(train$Cabin), ignore.case = T) == T)] <- d
 }
 train$Cabin <- as.factor(train$Cabin)
+rm(d)
+rm(decks)
 
 
 test$Cabin <- as.character(test$Cabin)
@@ -34,14 +37,16 @@ for(d in decks) {
   test$Cabin[which(grepl(pattern = paste(d, "+", sep = ''), x = as.character(test$Cabin), ignore.case = T) == T)] <- d
 }
 test$Cabin <- as.factor(test$Cabin)
-
 rm(d)
 rm(decks)
 
 
 
 
-################# impute age #################
+
+
+
+################# get titles #################
 train$Young <- F
 train$Young[which(grepl(pattern = paste("+", "Master\\.", "+", sep = ''), x = as.character(train$Name), ignore.case = T) == T)] <- T
 train$Young[which(grepl(pattern = paste("+", "Miss\\.", "+", sep = ''), x = as.character(train$Name), ignore.case = T) == T)] <- T
@@ -66,33 +71,67 @@ train$Title <- as.factor(getTitle(data = train))
 test$Title <-  as.factor(getTitle(data = test))
 
 
+
+
+
+
+
+
+############## get high class ###########
+high.class <- c("the Countess", "Sir", "Mme", "Mlle", "Dr")
+train$Title <- as.character(train$Title)
+train$Title[which(train$Title %in% high.class)] <- "highclass"
+
+test$Title <- as.character(test$Title)
+test$Title[which(test$Title %in% high.class)] <- "highclass"
+
+train$Title <- as.factor(train$Title)
+test$Title <- as.factor(test$Title)
+levels(test$Title) <- levels(train$Title)
+rm(high.class)
+
+
+
+
+
+
+
+########### get alone ####################
 train$Alone <- ifelse(test = train$SibSp == 0 & train$Parch == 0, yes = T, no = F)
 test$Alone <- ifelse(test = test$SibSp == 0 & test$Parch == 0, yes = T, no = F)
 
 
-summary(lm(Age~Title, data = train))
-randomForest(Survived~Title+Sex+Pclass+Age+Young+SibSp+Parch+Alone, data = train, ntree = 2000, importance = T)
 
+
+
+
+
+########### impute age ####################
 temp <- rbind(train, test)
-temp <- temp[, c("Age", "Pclass", "SibSp", "Young", "Sex", "Title", "Alone")]
-
-age.predicts <- c()
-for(i in 1:100) {
+imputeAge <- function(boots = 100, in.ratio = 0.66, temp) {
+  age.predicts <- c()
   train.temp <- temp[!is.na(temp$Age), ]
   test.temp <- temp[is.na(temp$Age), ]
   
-  rf.imput <- randomForest(y = train.temp$Age, x = train.temp[, -1], ntree = 2000, na.action = na.omit)
-  x <- predict(object = rf.imput, newdata = test.temp[, -1], type = "response")
-  age.predicts <- rbind(age.predicts, x)
-  
-  cat("i:", i, "\n")
-  
+  for(i in 1:boots) {
+    s <- sample(x = 1:nrow(train.temp), size = round(x = nrow(train.temp)*in.ratio, digits = 0), replace = T)
+    train.current <- train.temp[s, ]
+    
+    rf.imput <- randomForest(Age~Pclass+Parch+SibSp+Young+Title+Fare+Embarked, data = train.current, ntree = 3000, na.action = na.omit)
+    x <- predict(object = rf.imput, newdata = test.temp[, -1], type = "response")
+    age.predicts <- rbind(age.predicts, x)
+    
+    cat("i:", i, "\n")
+  }
+  return(age.predicts)
 }
-temp$Age[which(is.na(temp$Age))] <- apply(age.predicts, MARGIN = 2, FUN = median)
-
 
 
 #updated train
+train.temp <- temp[!is.na(temp$Age), ]
+test.temp <- temp[is.na(temp$Age), ]
+age.predicts <- imputeAge(boots = 100, in.ratio = 0.66, temp = temp)
+temp$Age[which(is.na(temp$Age))] <- apply(age.predicts, MARGIN = 2, FUN = median)
 train$Age <- temp$Age[1:891]
 test$Age <- temp$Age[892:1309]
 
@@ -106,10 +145,163 @@ rm(i)
 
 
 
+
+
+
+
+################# impute cabin ###############
+
+imputeCabin <- function(boots = 100, in.ratio = 0.66, train, test) {
+  temp <- rbind(train, test)
+  temp <- temp[, c("Cabin", "Pclass", "Fare", "Embarked")]
+  temp$Cabin <- as.character(temp$Cabin)
+  
+  train.temp <- temp[temp$Cabin != "", ]
+  test.temp <- temp[temp$Cabin == "", ]
+  
+
+  cabin.predicts <- c()
+  for(i in 1:boots) {
+    s <- sample(x = 1:nrow(train.temp), size = round(x = nrow(train.temp)*in.ratio, digits = 0), replace = T)
+    train.current <- train.temp[s, ]
+    train.current$Cabin <- as.factor(as.character(train.current$Cabin))
+    
+    rf.imput <- randomForest(Cabin~., data = train.current, ntree = 2000, na.action = na.omit)
+    x <- predict(object = rf.imput, newdata = test.temp[, -1], type = "response")
+    cabin.predicts <- rbind(cabin.predicts, as.character(x))
+    
+    cat("i:", i, "\n")
+  }
+  
+  max.cabin <- apply(cabin.predicts, MARGIN = 2, FUN = count)
+  final.cabins <- c()
+  for(mc in max.cabin) {
+    if(max(mc$freq) >= (0.7*boots)) {
+      c <- as.character(mc$x[which(mc$freq >= (0.7*boots))])
+    }
+    else {
+      c <- as.character(mc$x[which(mc$freq == max(mc$freq))[1]])
+    }
+    
+    final.cabins <- c(final.cabins, c)
+  }
+  
+  browser()
+  temp$Cabin <- as.character(temp$Cabin)
+  temp$Cabin[temp$Cabin == ""] <- final.cabins
+  return(temp)
+}
+
+test$Fare[153] <- 8
+imputed.cabins <- imputeCabin(boots = 200, in.ratio = 0.66, train = train, test = test)
+train$Cabin <- imputed.cabins$Cabin[1:891]
+test$Cabin <- imputed.cabins$Cabin[892:1309]
+rm(imputed.cabins)
+
+train$Cabin <- as.factor(as.character(train$Cabin))
+test$Cabin <- as.factor(as.character(test$Cabin))
+
+
+
+
+
+############# ticked parse ##########
+train$Ticket <- as.character(train$Ticket)
+test$Ticket <- as.character(test$Ticket)
+
+train$TicketNew <- NA
+for(t in 1:nrow(train)) {
+  new.ticket <- unlist(strsplit(train$Ticket[t], split = " "))
+  train$TicketNew[t] <- new.ticket[1]
+}
+
+test$TicketNew <- NA
+for(t in 1:nrow(test)) {
+  new.ticket <- unlist(strsplit(test$Ticket[t], split = " "))
+  
+  if(length(new.ticket) > 1) {
+    test$TicketNew[t] <- new.ticket[1]
+  }
+  else {
+    test$TicketNew[t] <- "other"
+  }
+}
+
+rm(new.ticket)
+rm(t)
+
+
+
+
+
+
+
+############## impute embarked ##############
+
+imputeEmbarked <- function(boots = 100, in.ratio = 0.66, train, test) {
+  temp <- rbind(train, test)
+  temp$Embarked <- as.character(temp$Embarked)
+
+  temp <- temp[, c("Embarked", "Cabin", "Alone", "SibSp", "Parch")]
+  
+  train.temp <- temp[temp$Embarked != "", ]
+  test.temp <- temp[temp$Embarked == "", ]
+  temp$Embarked <- as.factor(as.character(temp$Embarked))
+  
+  browser()
+  
+  embarked.predicts <- c()
+  for(i in 1:boots) {
+    s <- sample(x = 1:nrow(train.temp), size = round(x = nrow(train.temp)*in.ratio, digits = 0), replace = T)
+    train.current <- train.temp[s, ]
+    train.current$Embarked <- as.factor(as.character(train.current$Embarked))
+    
+    rf.imput <- randomForest(Embarked~Cabin+Alone+SibSp+Parch, data = train.current, ntree = 2000, na.action = na.omit)
+    x <- predict(object = rf.imput, newdata = test.temp[, -1], type = "response")
+    embarked.predicts <- rbind(embarked.predicts, as.character(x))
+    
+    cat("i:", i, "\n")
+  }
+  
+  browser()
+  
+  max.embarked <- apply(embarked.predicts, MARGIN = 2, FUN = count)
+  final.embarked <- c()
+  for(me in max.embarked) {
+    if(max(me$freq) >= (0.7*boots)) {
+      c <- as.character(me$x[which(me$freq >= (0.7*boots))])
+    }
+    else {
+      c <- as.character(me$x[which(me$freq == max(me$freq))[1]])
+    }
+    
+    final.embarked <- c(final.embarked, c)
+  }
+  
+  browser()
+  temp$Embarked <- as.character(temp$Embarked)
+  temp$Embarked[temp$Embarked == ""] <- final.embarked
+  return(temp)
+}
+
+imputed.embarked <- imputeEmbarked(boots = 200, in.ratio = 0.66, train = train, test = test)
+train$EmbarkedNew <- imputed.embarked$Embarked[1:891]
+test$EmbarkedNew <- imputed.embarked$Embarked[892:1309]
+rm(imputed.embarked)
+
+train$EmbarkedNew <- as.factor(as.character(train$EmbarkedNew))
+test$EmbarkedNew <- as.factor(as.character(test$EmbarkedNew))
+
+
+
+
+
+
 ################ relevel ###############
 levels(test$Sex) <- levels(train$Sex)
 levels(test$cabin.binary) <- levels(train$cabin.binary)
 levels(test$Embarked) <- levels(train$Embarked)
+levels(test$EmbarkedNew) <- levels(train$EmbarkedNew)
 levels(test$CabinDiscrete) <- levels(train$CabinDiscrete)
 levels(test$Survived) <- levels(train$Survived)
 
@@ -117,208 +309,151 @@ train$Young <- as.factor(train$Young)
 test$Young <- as.factor(test$Young)
 levels(test$Young) <- levels(train$Young)
 levels(test$Cabin) <- levels(train$Cabin)
-levels(test$Title) <- levels(train$Title)
+levels(train$Title) <- levels(test$Title)
+levels(train$Cabin) <- levels(test$Cabin)
+
 levels(test$Alone) <- levels(train$Alone)
 
 
+
+
+
+
+
+
+
+
 ################# remove bad cols #################
-train$Name <- NULL
-test$Name <- NULL
+# train$Name <- NULL
+# test$Name <- NULL
+# 
+# train$Ticket <- NULL
+# test$Ticket <- NULL
+# 
+# train$PassengerId <- NULL
+# test$PassengerId <- NULL
 
-train$Ticket <- NULL
-test$Ticket <- NULL
 
-train$PassengerId <- NULL
-test$PassengerId <- NULL
-
+save(train, file = "train1.RData")
+save(test, file = "test1.RData")
 
 
 ############################ svm #########################
 
-# tune svm params
-tuned <- tune.svm(Survived~Title+Sex+Pclass+Age+Young+SibSp+Parch+Cabin+Alone, data = train, type = "C-classification", gamma = 10^(-6:-1), cost = 10^(-2:2))
+load(file = "train.RData")
+load(file = "test.RData")
+
+z <- rbind(train, test)
+z$Cabin <- as.factor(as.character(z$Cabin))
+z$TicketNew <- as.factor(as.character(z$TicketNew))
+z$EmbarkedNew <- as.factor(as.character(z$EmbarkedNew))
+
+
+# # tune svm params
+tuned <- tune.svm(Survived~Title+Sex+Pclass+Age+SibSp+Parch+Embarked+Cabin, data = z[1:891, ], type = "C-classification", gamma = 10^(seq(from = -1, to = 1, by = 0.25)), cost = 10^(seq(from = -1, to = 1, by = 0.25)))
 summary(tuned)
-rm(tuned)
+# rm(tuned)
+# 
+# 
+# is.numeric(z$TicketNew[2])
+# 
+# save(train, file = "train.cabin.RData")
+# save(test, file = "test.cabin.RData")
 
 
-# tune predictors (effect of removing predictor)
-# tunes <- c()
-# for(c in colnames(train)[-1]) {
-#   temp <- new.train[, -which(colnames(train) %in% c)]
-#   tuned <- tune.svm(Survived~., data = temp, type = "C-classification", gamma = 10^(-6:-1), cost = 10^(-1:1))
-#   
-#   tunes <- rbind(tunes, c(c, tuned$best.performance))
-# }
 
 
 # learn
-svm <- e1071::svm(Survived~Title+Sex+Pclass+Age+Young+SibSp+Parch+Cabin, data = train, type = "C-classification", cost = 1, gamma = 0.1, cross = 500)
-predict <- predict(object = svm, newdata = train, type = "response")
-table(predict, train$Survived)
-rm(predict)
+svm <- e1071::svm(Survived~Title+Sex+Pclass+Age+SibSp+Parch+Embarked+Cabin, data = z[1:891, ], type = "C-classification", cost = 0.1778279, gamma = 0.1, kernel="radial") #0.1778279 #0.3162
+length(which(as.numeric(as.character(svm$fitted)) != as.numeric(as.character(z$Survived[1:891]))))
+
+# svm <- e1071::svm(Survived~Title+Sex+Pclass+Age+SibSp+Parch+EmbarkedNew+Cabin+TicketNew, data = z[1:891, ], type = "C-classification", cost = 0.17, gamma = 0.1, kernel="radial")
+# length(which(as.numeric(as.character(svm$fitted)) != as.numeric(as.character(z$Survived[1:891]))))
 
 
 
-compositeTraining <- function() {
-  
-  crossValidationLearning <- function(data, boots, ratio) {
-    indices <- 1:nrow(data)
-    sample.size <- round(x = length(indices)*ratio, digits = 0)
-    result.svm <- c()
-    result.rf <- c()
+predict.svm <- predict(object = svm, newdata = z[892:1309, ], type = "response")
+o <- read.csv(file = "0.799.csv")
+which(as.numeric(as.character(predict.svm)) != o$Survived)
+rm(o)
+
+
+predict.svm <- predict(object = svm, newdata = z[892:1309, ], type = "response")
+l <- read.csv(file = "newest.csv")
+which(as.numeric(as.character(predict.svm)) != l$Survived)
+rm(l)
+
+
+
+# export
+export.svm <- data.frame(PassengerId = pass.id, Survived = predict.svm, row.names = NULL)
+which(is.na(export.svm$Survived))
+write.table(export.svm, file = "newest2.csv", quote = F, sep = ",", row.names = F, col.names = T)
+
+
+
+
+bootstrapSurvived <- function(boots = 100, in.ratio = 0.66, z) {
+  predicted.survival <- c()
+  for(i in 1:boots) {
+    s <- sample(x = 1:891, size = round(x = 891*in.ratio, digits = 0), replace = T)
     
-    for(b in 1:boots) {
-      sampled.indices <- sample(x = indices, size = sample.size, replace = F)
-      temp <- data[sampled.indices, ]
-      
-      svm <- e1071::svm(Survived~., data = temp, type = "C-classification", cost = 1, gamma = 0.1)
-      rf <- randomForest(Survived~., data = temp, ntree = 2000)
-      bad.observations.svm <- which(as.numeric(as.character(svm$fitted))-as.numeric(as.character(temp$Survived)) != 0)
-      bad.observations.rf <- which(as.numeric(as.character(rf$predicted))-as.numeric(as.character(temp$Survived)) != 0)
-      result.svm <- c(result.svm, bad.observations.svm)
-      result.rf <- c(result.rf, bad.observations.rf)
-      
-      cat(b, "\n")
-    }
+    svm <- e1071::svm(Survived~Title+Sex+Pclass+Age+SibSp+Parch+Embarked+Cabin, data = z[s, ], 
+                      type = "C-classification", cost = 0.3162, gamma = 0.1, kernel="radial")
+    predict.svm <- predict(object = svm, newdata = z[892:1309, ], type = "response")
+    predicted.survival <- rbind(predicted.survival, as.character(predict.svm))
     
-    result <- list(result.rf = result.rf, result.svm = result.svm)
-    return (result)
+    cat("i:", i, "\n")
   }
   
+  max.survival <- apply(predicted.survival, MARGIN = 2, FUN = count)
+  final.survival <- c()
+  final.freq <- c()
+  for(mc in max.survival) {
+    if(max(mc$freq) >= (0.7*boots)) {
+      c <- as.character(mc$x[which(mc$freq >= (0.7*boots))])
+    }
+    else {
+      c <- as.character(mc$x[which(mc$freq == max(mc$freq))[1]])
+    }
+    
+    f <- (max(mc$freq)[1])/boots
+    
+    final.freq <- c(final.freq, f)
+    final.survival <- c(final.survival, c)
+  }
   
-  cvl <- crossValidationLearning(data = train, boots = 100, ratio = 0.5)
-  cvl.freq.rf <- plyr::count(cvl$result.rf)
-  cvl.freq.rf <- cvl.freq.rf[order(cvl.freq.rf$freq, decreasing = T), ]
-  
-  cvl.freq.svm <- plyr::count(cvl$result.svm)
-  cvl.freq.svm <- cvl.freq.svm[order(cvl.freq.svm$freq, decreasing = T), ]
-  rm(cvl)
-  
-  plot(cvl.freq.rf$freq)
-  plot(cvl.freq.svm$freq)
-  intersect(cvl.freq.rf$x, cvl.freq.svm$x)
-  
-  
-  bad <- train[cvl.freq.svm$x, ]
-  bad <- train[cvl.freq.svm$freq > 20, ]
-  table(bad[, c(1,3)])
-  table(train[, c(1,3)])
-  
-  
+  result <- list(final.survival = final.survival, final.freq = final.freq)
   
   
   
-  
-  temp.train <- train
-  temp.test <- test
-  
-  rf <- randomForest(Survived~., data = train, importance = T, ntree = 2000)
-  rf$importance
-  
-  svm <- e1071::svm(Survived~., data = train, type = "C-classification", cost = 1, gamma = 0.1)
-  predict <- predict(object = svm, newdata = train, type = "response")
-  table(predict, train$Survived)
-  rm(predict)
-  
-  
-  train$Embarked <- NULL
-  test$Embarked <- NULL
-  
-  train$Parch <- NULL
-  test$Parch <- NULL
-  
-  
-  
-#   train$Fare <- NULL
-#   test$Fare <- NULL
-#   
-#   train$Young <- NULL
-#   test$Young <- NULL
-#   
-#   train$SibSp <- NULL
-#   test$SibSp <- NULL
-  
-  cvl <- crossValidationLearning(data = train, boots = 1000, ratio = 0.5)
-  cvl.freq <- plyr::count(cvl)
-  cvl.freq <- cvl.freq[order(cvl.freq$freq, decreasing = T), ]
-  rm(cvl)
-  
-  
-  
-  train.bad <- train[cvl.freq$x, ]
-  train.bad$Pclass <- NULL
-  
-  
-  rf <- randomForest(Survived~., data = train.bad, importance = T, ntree = 2000)
-  svm <- e1071::svm(Survived~., data = train.bad, type = "C-classification", cost = 1, gamma = 0.1)
-  cvl <- crossValidationLearning(data = train.bad, boots = 1000, ratio = 0.5)
-  cvl.freq <- plyr::count(cvl)
-  cvl.freq <- cvl.freq[order(cvl.freq$freq, decreasing = T), ]
-  rm(cvl)
-  
-  
-  predict <- predict(object = svm, newdata = train.bad, type = "response")
-  table(predict, train.bad$Survived)
-  rm(predict)
-  
-  train.bad <- train[cvl.freq$x, ]
-  table(train.bad[, c(1, 3)])
-  cvl <- crossValidationLearning(data = train.bad, boots = 1000, ratio = 0.5)
-  cvl.freq <- plyr::count(cvl)
-  cvl.freq <- cvl.freq[order(cvl.freq$freq, decreasing = T), ]
-  rm(cvl)
-  
-  
-  
+  return (result)
 }
 
 
 
-svm <- e1071::svm(Survived~Title+Sex+Pclass+Age+Young+SibSp+Parch+Alone, data = train, type = "C-classification", cost = 10, gamma = 0.01)
-predict <- predict(object = svm, newdata = train, type = "response")
-table(predict, train$Survived)
-rm(predict)
+boot.survival <- bootstrapSurvived(boots = 500, in.ratio = 0.66, z)
 
 
-which(as.numeric(as.character(svm$fitted))-as.numeric(as.character(train$Survived)) != 0)
-bad <- train[which(as.numeric(as.character(svm$fitted))-as.numeric(as.character(train$Survived)) != 0), ]
 
-# get bad data
-predict.svm <- predict(object = svm, newdata = test[, -9], type = "response")
-predict.svm <- as.numeric(as.character(predict.svm))
+boot.survival$final.survival
+which(as.numeric(boot.survival$final.survival) != o$Survived)
+which(as.numeric(boot.survival$final.survival) != l$Survived)
 
 # export
-export.svm <- data.frame(PassengerId = pass.id, Survived = predict.svm, row.names = NULL)
-write.table(export.svm, file = "svm.csv", quote = F, sep = ",", row.names = F, col.names = T)
+export.svm <- data.frame(PassengerId = pass.id, Survived = as.numeric(boot.survival$final.survival), row.names = NULL)
+which(is.na(export.svm$Survived))
+write.table(export.svm, file = "newest3.csv", quote = F, sep = ",", row.names = F, col.names = T)
 
 
 
 
 
-rf <- randomForest(Survived~Title+Sex+Pclass+Age+Young+SibSp+Parch+Alone, data = train, ntree = 2000, importance = T)
-predict.rf <- predict(object = rf, newdata = test[, -9], type = "response")
-predict.rf <- as.numeric(as.character(predict.rf))
+plot(boot.survival$final.freq)
+plot(test$Title, boot.survival$final.freq)
 
-
-export.rf <- data.frame(PassengerId = pass.id, Survived = predict.rf, row.names = NULL)
-write.table(export.rf, file = "rf.csv", quote = F, sep = ",", row.names = F, col.names = T)
+plot(test$Title, boot.survival$final.survival)
 
 
 
 
-which(predict.rf != predict.svm)
-
-which(predict.svm != predict.rf)
-
-intersect(predict.rf, predict.svm)
-
-rf.prob <- predict(object = rf, newdata = test[, -9], type = "prob")
-predict(object = svm, newdata = test[, -9], type = "prob")
-
-
-which(predict.rf == predict.svm)
-which(predict.rf == predict.svm)
-
-predict.rf[which(rf.prob[, 1] < 0.2)] <- predict.svm[which(rf.prob[, 1] < 0.2)]
-
-which(predict.rf == 1 & predict.svm == 0)
